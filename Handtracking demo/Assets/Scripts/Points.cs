bafb02a393d18ed9e32c1f3bcd9a2e657ab3c3e9
Bart de Boer <bartdb2001@gmail.com>
2022-03-21 10:40:46 +0100

    Starting on script for hitboxes, build quality changes

---
+++ Handtracking demo/Assets/Scripts/Points.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    public GameObject hit20_1;
+    public GameObject hit20_2;
+    public GameObject hitdouble20;
+    public GameObject hittriple20;
+    public GameObject hit5_1;
+    public GameObject hit5_2;
+    public GameObject hitdouble5;
+    public GameObject hittriple5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
