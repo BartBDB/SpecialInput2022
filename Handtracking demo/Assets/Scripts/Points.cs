a4d5cb965b24ccff4d30d899ae9bf12d7ed5ca2a
Bart de Boer <bartdb2001@gmail.com>
2022-03-22 10:34:01 +0100

    Hitbox setup

--- Handtracking demo/Assets/Scripts/Points.cs
+++ Handtracking demo/Assets/Scripts/Points.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Points : MonoBehaviour
 {
-    public GameObject hit20_1;
-    public GameObject hit20_2;
-    public GameObject hitdouble20;
-    public GameObject hittriple20;
-    public GameObject hit5_1;
-    public GameObject hit5_2;
-    public GameObject hitdouble5;
-    public GameObject hittriple5;
+    public GameObject hit1;
+    public GameObject hit2;
+    public GameObject hit3;
+    public GameObject hit4;
+    public GameObject hit5;
+    public GameObject hit6;
+    public GameObject hit7;
+    public GameObject hit8;
+    public GameObject hit9;
+    public GameObject hit10;
+    public GameObject hit11;
+    public GameObject hit12;
+    public GameObject hit13;
+    public GameObject hit14;
+    public GameObject hit15;
+    public GameObject hit16;
+    public GameObject hit17;
+    public GameObject hit18;
+    public GameObject hit19;
+    public GameObject hit20;
+    public GameObject dart;
 
     // Start is called before the first frame update
     void Start()
