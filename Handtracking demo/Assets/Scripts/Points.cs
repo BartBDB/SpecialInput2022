baa41d9044c5af15de739f243c3f5cfb9adc1855
Bart de Boer <bartdb2001@gmail.com>
2022-03-28 10:33:22 +0200

    TMPro + gifs for readme

--- Handtracking demo/Assets/Scripts/Points.cs
+++ Handtracking demo/Assets/Scripts/Points.cs
@@ -1,40 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Points : MonoBehaviour
 {
-    public GameObject hit1;
-    public GameObject hit2;
-    public GameObject hit3;
-    public GameObject hit4;
-    public GameObject hit5;
-    public GameObject hit6;
-    public GameObject hit7;
-    public GameObject hit8;
-    public GameObject hit9;
-    public GameObject hit10;
-    public GameObject hit11;
-    public GameObject hit12;
-    public GameObject hit13;
-    public GameObject hit14;
-    public GameObject hit15;
-    public GameObject hit16;
-    public GameObject hit17;
-    public GameObject hit18;
-    public GameObject hit19;
-    public GameObject hit20;
     public GameObject dart;
+    public TextMeshPro text;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        text.text = "";
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision dataFromCollision)
     {
-        
+        if (dataFromCollision.gameObject.name == "1")
+        {
+            text.text = "1";
+        }
+
+        if (dataFromCollision.gameObject.name == "2")
+        {
+            text.text = "2";
+        }
+
+        if (dataFromCollision.gameObject.name == "3")
+        {
+            text.text = "3";
+        }
+
+        if (dataFromCollision.gameObject.name == "4")
+        {
+            text.text = "4";
+        }
+
+        if (dataFromCollision.gameObject.name == "5")
+        {
+            text.text = "5";
+        }
+
+        if (dataFromCollision.gameObject.name == "6")
+        {
+            text.text = "6";
+        }
+
+        if (dataFromCollision.gameObject.name == "7")
+        {
+            text.text = "7";
+        }
+
+        if (dataFromCollision.gameObject.name == "8")
+        {
+            text.text = "8";
+        }
+
+        if (dataFromCollision.gameObject.name == "9")
+        {
+            text.text = "9";
+        }
+
+        if (dataFromCollision.gameObject.name == "10")
+        {
+            text.text = "10";
+        }
+
+        if (dataFromCollision.gameObject.name == "11")
+        {
+            text.text = "11";
+        }
+
+        if (dataFromCollision.gameObject.name == "12")
+        {
+            text.text = "12";
+        }
+
+        if (dataFromCollision.gameObject.name == "13")
+        {
+            text.text = "13";
+        }
+
+        if (dataFromCollision.gameObject.name == "14")
+        {
+            text.text = "14";
+        }
+
+        if (dataFromCollision.gameObject.name == "15")
+        {
+            text.text = "15";
+        }
+
+        if (dataFromCollision.gameObject.name == "16")
+        {
+            text.text = "16";
+        }
+
+        if (dataFromCollision.gameObject.name == "17")
+        {
+            text.text = "17";
+        }
+
+        if (dataFromCollision.gameObject.name == "18")
+        {
+            text.text = "18";
+        }
+
+        if (dataFromCollision.gameObject.name == "19")
+        {
+            text.text = "19";
+        }
+
+        if (dataFromCollision.gameObject.name == "20")
+        {
+            text.text = "20";
+        }
     }
 }
